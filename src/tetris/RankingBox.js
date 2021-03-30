bfe871ddb2f9ade50ca2087187e6cb2c409857d7
kwangsun <eyeyel@naver.com>
2021-03-30 23:13:18 +0900

    Ranking ui modification

--- src/tetris/RankingBox.js
+++ src/tetris/RankingBox.js
@@ -10,7 +10,7 @@ function RankingBox({onBack}){
             return res.status === 200 ? res.json() : []; 
         })
         .then(res => {
-            const sorted = res.sort((a, b)=> a.score - b.score);
+            const sorted = res.sort((a, b)=> b.score - a.score);
             setRankList(sorted);
         })
         .catch(err => console.error(err));
@@ -21,15 +21,25 @@ function RankingBox({onBack}){
     }, []);
     
     return (
-        <div className="rank-box">
-            <div style={{height : "40vh"}}>
-                {rankList.map((rank, idx) => {
-                    if(idx === 0) rank.link = '/rank1.png';
-                    return rank ? <RankingList rank={rank} key={rank.name}></RankingList> : null;
-                })}
+        <>
+            <div className="rank-header">
+                <div>Rank</div>
+                <div className="rank-name">Name</div>
+                <div>Score</div>
+                <div style={{margin:"0 auto"}}></div>
+                <div>Title</div>
+            </div>
+            <div className="rank-box">
+                <div style={{height : "45vh"}}>
+                    {rankList.map((rank, idx) => {
+                        if(idx === 0) rank.link = '/rank1.png';
+                        rank.rank = idx + 1;
+                        return rank ? <RankingList rank={rank} key={rank.name}></RankingList> : null;
+                    })}
+                </div>
             </div>
             <div className="label1" onClick={() => onBack()}>Back</div>
-        </div>
+        </>
     );
 }
 
--- src/tetris/RankingList.js
+++ src/tetris/RankingList.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 function RankingList({rank}){
+    const timerId = useRef(0);
+    const topRef = useRef();
+
+    useEffect(()=>{
+        setInterval(()=>{
+            if(topRef.current){
+                topRef.current.parentNode.classList.toggle("rank-top");
+            }
+        }, 1000);
+        return ()=>clearInterval(timerId.current);
+    }, []);
 
     return (
         <div className="rank-row">
+            <div className="rank-rank">{rank.rank}</div>
             <div className="rank-name">{rank.name}</div>
             <div className="rank-score">{rank.score}</div>
             {rank.link ? <div style={{margin:"0 auto"}}></div> : null}
-            {rank.link ? <img src={rank.link} style={{width:"25px"}}></img> : null}
+            {rank.link ? <img src={rank.link} ref={topRef} style={{width:"25px"}}></img> : null}
         </div>
     );
 }
